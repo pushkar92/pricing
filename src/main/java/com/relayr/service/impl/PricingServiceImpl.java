a8700f693373a4fc8be88fbcfbb954dd065909bb
pushkar92 <57790464+pushkar92@users.noreply.github.com>
2020-11-03 13:39:41 +0530

    Create PricingServiceImpl.java

---
+++ src/main/java/com/relayr/service/impl/PricingServiceImpl.java
@@ -0,0 +1,139 @@
+package com.relayr.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.service.spi.ServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.relayr.model.PricingModel;
+import com.relayr.model.Product;
+import com.relayr.model.Product_Store_Details;
+import com.relayr.model.Store;
+import com.relayr.repo.ProductRepository;
+import com.relayr.repo.ProductStoreDetailRepositroy;
+import com.relayr.repo.StoreRepository;
+import com.relayr.service.PricingService;
+
+@Service
+public class PricingServiceImpl implements PricingService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Autowired
+	private ProductStoreDetailRepositroy productStoreDetailRepositroy;
+
+	@Autowired
+	private StoreRepository storeRepository;
+
+	@Override
+	public Iterable<PricingModel> findByProductName(String productName) throws ServiceException {
+		List<Product> productList = productRepository.findByName(productName);
+
+		List<PricingModel> pricingList = new ArrayList<>();
+
+		for (Product product : productList) {
+			Long productId = product.getId();
+			List<Product_Store_Details> productStoreList = productStoreDetailRepositroy.findByProductId(productId);
+			String category = product.getCategory();
+			PricingModel pricingModel = null;
+			for (Product_Store_Details productStore : productStoreList) {
+				String link = productStore.getLink();
+				Store store = storeRepository.findById(productStore.getStoreId()).get();
+				String storeName = store.getName();
+				double mrp = productStore.getMrp();
+				double sp = productStore.getSp();
+				double discount = productStore.getDiscount();
+				int rating = productStore.getRating();
+				pricingModel = new PricingModel(productName, storeName, link, mrp, sp, discount, category, rating);
+			}
+
+			if (pricingModel != null) {
+				pricingList.add(pricingModel);
+			}
+		}
+		return pricingList;
+
+	}
+
+	@Override
+	public Iterable<PricingModel> findByProductAndCategory(String productName, String category)
+			throws ServiceException {
+		List<Product> productList = productRepository.findByNameAndCategory(productName, category);
+		
+		List<PricingModel> pricingList = new ArrayList<>();
+
+		for (Product product : productList) {
+			Long productId = product.getId();
+			List<Product_Store_Details> productStoreList = productStoreDetailRepositroy.findByProductId(productId);
+			
+			PricingModel pricingModel = null;
+			for (Product_Store_Details productStore : productStoreList) {
+				String link = productStore.getLink();
+				Store store = storeRepository.findById(productStore.getStoreId()).get();
+				String storeName = store.getName();
+				double mrp = productStore.getMrp();
+				double sp = productStore.getSp();
+				double discount = productStore.getDiscount();
+				int rating = productStore.getRating();
+				pricingModel = new PricingModel(productName, storeName, link, mrp, sp, discount, storeName, rating);
+				pricingList.add(pricingModel);
+			}
+		}
+		return pricingList;
+
+	}
+
+	@Override
+	public void savePricingModel(List<PricingModel> pricingModels) throws ServiceException {
+		for (PricingModel pricingModel : pricingModels) {
+			long productId = -1L;
+			long storeId = -1L;
+
+			if (null != pricingModel.getProductName() && null != pricingModel.getCategory()) {
+				productId = saveProduct(pricingModel.getProductName().toLowerCase(),
+						pricingModel.getCategory().toLowerCase());
+			}
+
+			if (null != pricingModel.getStoreName()) {
+				storeId = saveStore(pricingModel.getStoreName().toLowerCase());
+			}
+
+			if (storeId != -1 && productId != -1) {
+				Product_Store_Details productStore = new Product_Store_Details(pricingModel.getLink().toLowerCase(),
+						storeId, productId, pricingModel.getMrp(), pricingModel.getSp(), pricingModel.getDiscount(),
+						pricingModel.getRating());
+				productStoreDetailRepositroy.save(productStore);
+			}
+		}
+	}
+
+	private long saveProduct(String productName, String category) {
+		List<Product> product = productRepository.findByName(productName);
+		if (product.size() > 0) {
+			return product.get(0).getId();
+		} else {
+			Product product1 = productRepository.save(new Product(productName, category));
+			return product1.getId();
+		}
+	}
+
+	private long saveStore(String storeName) {
+		List<Store> store = storeRepository.findByName(storeName);
+		if (store.size() > 0) {
+			return store.get(0).getId();
+		} else {
+			Store store1 = storeRepository.save(new Store(storeName));
+			return store1.getId();
+		}
+	}
+
+	@Override
+	public Iterable<PricingModel> findAll(String PricingModelName) throws ServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
